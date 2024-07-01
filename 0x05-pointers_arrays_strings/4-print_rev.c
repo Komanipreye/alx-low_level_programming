022638f56837f2aa10ae23a41f1132f33990674c
Komani <preyekomani@gmail.com>
2024-07-01 01:37:32 -0700

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,13 +6,11 @@
  */
 void print_rev(char *s)
 {
-	int len = 0, index;
+	int len = strlen(s);
 
-	while (s[index++])
-		len++;
-
-	for (index = len - 1; index >= 0; index--)
-		_putchar(s[index]);
-
-	_putchar('\n');
+	for (int i = len - 1; i >= 0; i--)
+	{
+		printf("%c", s[i]);
+	}
+	printf("\n");
 }
