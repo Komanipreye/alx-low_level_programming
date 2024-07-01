cfabc01a5e7ca931dd4c0a5fb746b75d1af3a881
Komani <preyekomani@gmail.com>
2024-07-01 11:16:57 -0700

    string

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -7,15 +7,13 @@
  * Return: string
  */
 
-char *_strcpy(char *dest, char *src)
-
+void print_array(int *a, int n)
 {
-	char *guard = dest;
+	int i;
 
-	while (*src)
-	{
-		*dest++ = *src++;
-		*dest = 0;
-	}
-	return (guard);
+	for (i = 0; i < (n - 1); i++)
+		printf("%d, ", a[i]);
+		if (i == (n - 1))
+			printf("%d", a[n - 1]);
+	printf("\n");
 }
