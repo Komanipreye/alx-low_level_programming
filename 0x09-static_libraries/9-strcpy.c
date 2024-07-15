fdaf9ae18bb3f72176f8f79dfd4c33d7365e9b74
Komani <preyekomani@gmail.com>
2024-07-15 13:44:01 -0700

    task 9 fix

--- 0x09-static_libraries/9-strcpy.c
+++ 0x09-static_libraries/9-strcpy.c
@@ -6,11 +6,17 @@
  * Return: returns 0 success
  *
  */
+
 char *_strcpy(char *dest, char *src)
 {
-	char *tmp = dest;
+	int i;
 
-	while (*src)
-		*dest++ = *src++;
-	return (tmp);
+	i = 0;
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
 }
