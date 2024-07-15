b3a4f5fd2afe0486ec54fe2a85755a9ac228c588
Komani <preyekomani@gmail.com>
2024-07-15 14:19:06 -0700

    update

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,3 +1,9 @@
-#/bin/bash
-gcc -Wall -Werror -Wextra -pedantic -c *.c
-ar -rc liball.a *.o
+#!/bin/bash
+
+# a script that creates a static library
+# called liball.a from all the .c files
+# that are in the current directory
+
+gcc -c *.c
+ar rc liball.a *.o
+echo y | rm -I *.o
