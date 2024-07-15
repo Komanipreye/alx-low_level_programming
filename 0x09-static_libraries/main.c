63f0dea7a34a10c942af6ccfb3265952f8b08e7d
Komani <preyekomani@gmail.com>
2024-07-15 13:40:27 -0700

    fix

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
+
 int main(void)
 {
-	_puts("\"At the end of the day, my goal was just to learn the tools to make my job easier\"\n\t- Komani Preye");
-    return (0);
+	_puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
+	return (0);
 }
