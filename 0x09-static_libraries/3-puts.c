57a3850cf5193fa0c031e336437240d05887199a
Komani <preyekomani@gmail.com>
2024-07-16 05:41:04 -0700

    3-puts.c fix

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -2,11 +2,13 @@
 
 /**
  * _puts - Prints a string to stdout.
- *  @str: The string to be printed.
+ * @str: The string to be printed.
+ * _putchar prints a new line
  */
 void _puts(char *str)
 {
 	while (*str)
 		_putchar(*str++);
+
 	_putchar('\n');
 }
