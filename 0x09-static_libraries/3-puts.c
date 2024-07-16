577b7ceafc0b6177192a6581068a0ab858e633cf
Komani <preyekomani@gmail.com>
2024-07-15 23:11:01 -0700

    Library Update

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,13 +1,12 @@
 #include "main.h"
 
 /**
- *  * _puts - Prints a string to stdout.
- *   * @str: The string to be printed.
- *    */
+ * _puts - Prints a string to stdout.
+ *  @str: The string to be printed.
+ */
 void _puts(char *str)
 {
-		while (*str)
-					_putchar(*str++);
-
-			_putchar('\n');
+	while (*str)
+		_putchar(*str++);
+	_putchar('\n');
 }
