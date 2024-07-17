cd7103e7798288fd97821304f163e5e1e48f9cb6
Komani <preyekomani@gmail.com>
2024-07-16 21:13:18 -0700

    task 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,51 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-/**
- * num_checker - checks if a given char is number or not
- * @a: char to be checked
- * Return: 1, if its a number, else 0
- **/
-int num_checker(char *a)
-{
-	int i, num, len;
+#include <ctype.h>
+#include "main.h"
 
-	i = 0;
-	num = 0;
-	len = strlen(a);
-	while (i < len)
-	{
-		if (a[i] < '0' || a[i] > '9')
-		{
-			return (-1);
-		}
-		else
-			num = num * 10 + (a[i] - '0');
-		i++;
-	}
-	return (num);
-}
 /**
- * main - add positive numbers
- * @argc: arguement count
- * @argv: array of pointers to arguement strings
- * Return: result of addition or 1
- **/
+ * main - Program that takes in all integer arguments and returns the sum
+ * @argc: Number of command line arguments
+ * @argv: Array name
+ * Return: 1 if a non-integer is among the passed in arguments, 0 otherwise
+ */
+
 int main(int argc, char *argv[])
 {
-	int i, num, sum;
+	int i, j, length, sum;
+	char *ptr;
 
-	sum = 0;
-	for (i = 1; i < argc; i++)
+	if (argc < 2)
+		printf("0\n");
+	else
 	{
-		num = num_checker(argv[i]);
-		if (num == -1)
+		sum = 0;
+		for (i = 1; i < argc; i++)
 		{
-			printf("Error\n");
-			return (1);
+			ptr = argv[i];
+			length = strlen(ptr);
+
+			for (j = 0; j < length; j++)
+			{
+				if (isdigit(*(ptr + j)) == 0)
+				{
+					printf("Error\n");
+					return (1);
+				}
+			}
+
+			sum += atoi(argv[i]);
 		}
-		sum += num;
-	}
+
 	printf("%d\n", sum);
+	}
 	return (0);
 }
