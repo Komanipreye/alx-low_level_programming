7830887da5b9ff7aa07496c9de68604f29a2d03b
Tamunokepreye <komani49@gmail.com>
2024-08-01 18:12:16 +0400

    List Lenght fix

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,20 +1,20 @@
-#include "lists.h"
-#include <stddef.h>
 #include <stdlib.h>
+#include "lists.h"
+
 /**
- * list_len - finds the number of nodes in a list
- * @h: singly linked list
- * Return: number of elements in a linked list
+ * list_len - returns the number of elements in a linked list
+ * @h: pointer to the list_t list
+ *
+ * Return: number of elements in h
  */
 size_t list_len(const list_t *h)
-	{
-	size_t cont = 0;
+{
+	size_t n = 0;
 
 	while (h)
 	{
-		cont++;
+		n++;
 		h = h->next;
 	}
-
-	return (cont);
+	return (n);
 }
